9bbd117c256d5632d6d295a8784138a0bef4ca86
frocha <frocha@student.42heilbronn.de>
2025-03-31 22:59:52 +0200

    remove function names from header not needed there anymore

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -128,9 +128,6 @@ int distance_ray2wall(float origin_x, float origin_y, float destination_x, float
 void find_horz_ray_wall_hit(t_horz_wall_hit_data *horz_wall_hit_data, float ray_angle, t_player *player, t_ray_directin_data  *ray_directin_data);
 void find_vert_ray_wall_hit(t_vert_wall_hit_data *vert_wall_hit_data, float ray_angle, t_player *player, t_ray_directin_data  *ray_directin_data);
 void inititate_ray_direction_data(t_ray_directin_data *ray_directin_data, float ray_angle);
-void set_values_ray(t_rays *rays, float ray_angle, t_ray_directin_data  *ray_directin_data, int counter);
-void set_vert_values_ray(t_rays *rays, t_hit_distance_wall *hit_distance_wall, t_vert_wall_hit_data *vert_wall_hit_data, int counter);
-void set_horz_values_ray(t_rays *rays, t_hit_distance_wall *hit_distance_wall, t_horz_wall_hit_data *horz_wall_hit_data, int counter);
 void find_distance(t_hit_distance_wall *hit_distance_wall, t_horz_wall_hit_data *horz_wall_hit_data, t_vert_wall_hit_data *vert_wall_hit_data, t_player *player);
 void initiate_wall_hit_data_structs_values(t_horz_wall_hit_data *horz_wall_hit_data, t_vert_wall_hit_data *vert_wall_hit_data);
 
